b3c99e1e95ba15fde017679121a01f586486a633
John Kayne <johnhoganuke2@gmail.com>
2016-06-05 11:34:47 -0700

    Upload Gobject.java

---
+++ src/main/java/jogen/prototypes/world/Gobject.java
@@ -0,0 +1,12 @@
+package jogen.prototypes.world;
+
+import jogen.prototypes.meta.RenderClass;
+import jogen.prototypes.world.data.substance.Composition;
+import jogen.prototypes.world.data.Position;
+
+public class Gobject {
+  public String name;
+  public Composition madeOf;
+  public RenderClass render;
+  public Position position;
+}
